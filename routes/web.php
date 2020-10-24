a75b0741e62ddb6fef0ffe7a286548e2ab819baa
wenisumardi <wenipatungga12@gmail.com>
2020-10-24 16:27:55 +0700

    tugas2 done

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,23 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', function () {
+    return view('store.home');
+});
+
+Route::get('/login', function () {
+    return view('store.login');
+});
+
+Route::get('/register', function () {
+    return view('store.register');
+});
+
+Route::get('/produk', function () {
+    return view('store.produk');
+});
+
+Route::get('/produkditel', function () {
+    return view('store.produkditel');
+});
